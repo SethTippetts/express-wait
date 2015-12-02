6bb35d08874bdd1daa113c73dada8010eade13c4
Seth Tippetts <SethTippetts@users.noreply.github.com>
2015-12-02 14:15:51 -0700

    Spacing issue

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 var Promise = require('bluebird');
 var once = require('./lib/once');
 
-  var initCb;
-  var init = Promise.fromCallback(function(cb) { initCb = once(cb); });
+var initCb;
+var init = Promise.fromCallback(function(cb) { initCb = once(cb); });
 
 module.exports = function (req, res, next) {
   init.then(function() { next(); }).catch(next);
